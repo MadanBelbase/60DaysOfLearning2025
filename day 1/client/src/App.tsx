2b77b03bf0860f554e931c8f55f56b0bf71cc544
MadanBelbase <madanbelbase927@gmail.com>
2025-06-01 22:29:55 +0545

    day 1

---
+++ day 1/client/src/App.tsx
@@ -0,0 +1,12 @@
+import Homepage from './pages/Homepage'
+import Navbar from './components/Navbar'
+function App() {
+  return (
+    <div className="app">
+    <Navbar />
+      <Homepage />
+    </div>
+  )
+}
+
+export default App
---
+++ day 1/client/src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+import '../styles/Navbar.css';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        {/* Logo/Brand Name */}
+        <Link to="/" className="navbar-logo">
+          <span className="logo-icon">✍️</span>
+          <span className="logo-text">InkNode</span> 
+        </Link>
+
+        {/* Mobile Menu Toggle */}
+        <div className="menu-toggle">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+
+        {/* Navigation Links */}
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/blogs" className="nav-link">Blogs</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/create" className="nav-link">Create Post</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-link">About</Link>
+          </li>
+        </ul>
+
+        {/* Auth Buttons */}
+        <div className="auth-buttons">
+          <button className="login-btn">Login</button>
+          <button className="signup-btn">Sign Up</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
